332ab4edd1e47163ebad42987ec6b577e3e3b9ef
Brijesh Rana <brijesh.1995rana@gmail.com>
2019-05-17 18:32:36 +0530

    final commit

--- app/src/main/java/com/example/onlinestorekotlin/CartProductsActivity.kt
+++ app/src/main/java/com/example/onlinestorekotlin/CartProductsActivity.kt
@@ -1,12 +1,17 @@
 package com.example.onlinestorekotlin
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
 import android.widget.ArrayAdapter
+import android.widget.Toast
 import androidx.appcompat.app.AlertDialog
 import com.android.volley.Request
 import com.android.volley.Response
 import com.android.volley.toolbox.JsonArrayRequest
+import com.android.volley.toolbox.StringRequest
 import com.android.volley.toolbox.Volley
 import kotlinx.android.synthetic.main.activity_cart_products.*
 
@@ -41,5 +46,64 @@ class CartProductsActivity : AppCompatActivity() {
         })
 
         requestQ.add(jsonAR)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+
+        menuInflater.inflate(R.menu.cart_menu,menu)
+
+        return super.onCreateOptionsMenu(menu)
+    }
+
+
+    override fun onOptionsItemSelected(item: MenuItem?): Boolean {
+
+        if(item?.itemId == R.id.continueShoppingItem){
+
+            var intent = Intent(this,HomeScreen::class.java)
+            startActivity(intent)
+
+        }else if(item?.itemId == R.id.declineOrderItem){
+
+            var deleteUrl = "http://192.168.43.206/ECommerce/decline_order.php?email=${Person.email}"
+            var requestQ = Volley.newRequestQueue(this@CartProductsActivity)
+            var stringRequest = StringRequest(Request.Method.GET,deleteUrl,Response.Listener { response ->
+
+                var intent = Intent(this,HomeScreen::class.java)
+                startActivity(intent)
+
+            },Response.ErrorListener { error ->
+
+                val dialogBuilder = AlertDialog.Builder(this)
+                dialogBuilder.setTitle("Message").
+                    setMessage(error.message).create().show()
+
+            })
+
+            requestQ.add(stringRequest)
+
+        }else if(item?.itemId == R.id.verifyOrderItem){
+
+            var verifyOrderUrl = "http://192.168.43.206/OnlineStoreApp/verify_order.php?email=${Person.email}"
+            var requestQ = Volley.newRequestQueue(this)
+            var stringRequest = StringRequest(Request.Method.GET,verifyOrderUrl,Response.Listener { response ->
+
+                var intent = Intent(this,FinalizeShoppingActivity::class.java)
+                Toast.makeText(this,response,Toast.LENGTH_SHORT).show()
+                intent.putExtra("LATEST_INVOICE_NUMBER",response)
+                startActivity(intent)
+
+            },Response.ErrorListener { error ->
+
+                val dialogBuilder = AlertDialog.Builder(this)
+                dialogBuilder.setTitle("Message").
+                    setMessage(error.message).create().show()
+
+            })
+            requestQ.add(stringRequest)
+
+        }
+
+        return super.onOptionsItemSelected(item)
     }
 }
---
+++ app/src/main/java/com/example/onlinestorekotlin/MyPaypal.kt
@@ -0,0 +1,9 @@
+package com.example.onlinestorekotlin
+
+class MyPaypal {
+
+    companion object{
+
+        var clientID = "AbUxK5zJutD-99wJpDORf1YDADMKUGTRs63X7uvoxckl4TF16i-VmN3fw4jNuwYVLTONGyElfnibuG88"
+    }
+}
